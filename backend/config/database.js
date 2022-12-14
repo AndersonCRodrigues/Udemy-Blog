f56079c3434a6270520062ef1ee44061042b18d8
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-14 15:38:42 -0300

    add menuLink e footer

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,12 +2,16 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'lallah.db.elephantsql.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'oopehnss'),
-      user: env('DATABASE_USERNAME', 'oopehnss'),
-      password: env('DATABASE_PASSWORD', 'beh_bcehq7MWrlmz35V3f7SJyIoWL90E'),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432 ),
+      database: env('DATABASE_NAME', ''),
+      user: env('DATABASE_USERNAME', ''),
+      password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', true),
+    },
+    pool: {
+      min:0,
+      max:5,
     },
   },
 });
